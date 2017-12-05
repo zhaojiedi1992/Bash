b613f76bc84cd161765ac3f6eea30e7b68059a8d
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2017-12-05 12:55:00 +0800

    add readlink_while func to func.sh

--- func.sh
+++ func.sh
@@ -49,3 +49,24 @@ is_no(){
 	no_re="^[Nn]([Nn])?$"
 	test_re $1 $no_re
 }
+readlink_while(){
+	src=$1
+	while true ; do 
+		tmp=`readlink $src`
+		if [ -z "$tmp" ] ; then 
+			echo  $src
+			return 0
+		else
+			src=$tmp
+		fi
+	done
+}
+			
+
+
+
+
+
+
+
+
