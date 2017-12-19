94d585e8b93d9ac94f00d7ff382b19a55fd6a292
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2017-12-19 20:47:28 +0800

    chang set_yum.sh user a file

--- set_yum.sh
+++ set_yum.sh
@@ -3,11 +3,11 @@ if [ ! -f /etc/yum.repos.d/cdrom.repo ] ; then
 	cp files/cdrom.repo /etc/yum.repos.d/
 else 
 	mkdir /etc/yum.repos.d/bak -pv 
-	mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/bak
+	\mv /etc/yum.repos.d/*.repo /etc/yum.repos.d/bak
 fi
 
 
-if [ $RELEASE_VERSION -ge 7 ] ; then 
+if [ "$RELEASE_VERSION" -ge 7 ] ; then 
 	systemctl start autofs
 	systemctl enable autofs
 else 
