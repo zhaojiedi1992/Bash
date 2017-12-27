460dfa1a6182097058d15deff57e8e47973dcd08
zhaojiedi1992 <zhaojiedi1992@outlook.com>
2017-12-27 19:00:27 +0800

    add copy_cmd.sh

--- README.md
+++ README.md
@@ -1,13 +1,35 @@
 # Bash
 some spractical bash scripts
-## init.sh
+##  reset.sh
 init works
 
 ## func.sh
 some function
 
-## alias.sh
+## set_alias.sh
 some alias
 
-## scanscsi.sh
-scan scsi device
+## set_motd.sh
+set /etc/motd and /etc/motd.net file
+
+## set_netcard.sh
+set netcard from ens33 to eth0 
+
+## set_path
+set RELEASE_VERSION and some VAR
+
+## set_selinux_and_firewalld.sh
+stop selinux and stop firewalld
+
+## set_vimrc.sh 
+copy a vimrc file to ~/.vimrc 
+
+## set_yum.sh 
+set yum info , and install some packages.
+
+## copy_path 
+copy path
+
+
+
+
--- bin/copy_cmd.sh
+++ bin/copy_cmd.sh
@@ -8,6 +8,8 @@
 #Other      :
 #================================================
 dst=/mnt/sysimage
+overwrite=yes
+
 if [ ! -e "$dst" ] ; then 
 	mkdir -pv $dst
 fi 
@@ -18,21 +20,34 @@ my_cp() {
 	if [ ! -e $dir ] ; then 
 		mkdir -pv $dir
 	fi 
-	\cp -a $src $dst
+
+	if [ ! -f "$dst"  -o "$overwrite" == "yes" ] ; then 
+		cp  $src $dst
+	fi
 }
 
+
 while true; do 
-	read -p "enter you cmd; " short_cmd
-	if [ "$short_cmd" == "quit" -o "$short_cmd" =="q" ] ; then 
+	read -p "enter you cmd :  " short_cmd
+	if [ "$short_cmd" == "quit" ] ; then 
 		exit 0
 	fi
-	short_cmd=`basename $short_cmd`
-	long_cmd=`which --skip-alias --skip-functions --skip-dot --skip-tilde $short_cmd` &>/dev/null
+	type $short_cmd 2>/dev/null |grep -q "builtin"
+	if [ "$?" -eq 0 ] ; then 
+		echo "this is a buildin cmd, nothing todo,  input 'bash' for copy buildin cmd" 
+		continue
+	fi
+	short_cmd=`basename $short_cmd` &>/dev/null
+	long_cmd=`which --skip-alias --skip-functions --skip-dot --skip-tilde $short_cmd 2>/dev/null`
+	if [ "$?" -ne 0 ] ; then 
+		echo "not a valid command, please check and try again" 
+		continue
+	fi
 	# cp bin
 	my_cp $long_cmd $dst$long_cmd
 	# cp lib 
-	for lib in `ldd $long_cmd |egrep -o  "/[^[:space:]]+ " ` ; do 
-		echo -e "\n$lib\n"
+	for lib in `ldd $long_cmd |egrep -o  "/[^[:space:]]+" ` ; do 
+		#echo -e "\n$lib\n"
 		my_cp $lib $dst$lib
 	done
 	# cp man 
